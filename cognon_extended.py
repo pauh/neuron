a1febefcbc98874bac97c17b769e1343341a94e5
Pau Haro Negre <pauharo@gmail.com>
2013-04-05 00:13:08 +0000

    Add initial code

---
+++ cognon_extended.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python2.7
+
+# Copyright 2013 Pau Haro Negre
+# based on C++ code by Carl Staelin Copyright 2009-2011
+# 
+# See the NOTICE file distributed with this work for additional information
+# regarding copyright ownership.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import numpy as np
+
+
+### CONSTANTS ###
+
+
+
+### CLASSES ###
+class Word(object):
+    """A Word contains a list of those input synapses that fired for the most
+    recent given excitation pattern.
+
+    Attributes:
+        offset: A set containing the syanpses that fired.
+    """
+    
+    def __init__(self, fired_syn=[]):
+        """Inits Word class.
+
+        Args:
+            fired_syn: List of input synapses that fired.
+        """
+        self.offset = set(fired_syn)
+
+
+class Neuron(object):
+    """Models a CB neuron.
+
+    Attributes:
+        S0: Number of synapses.
+        H: Number of synapses needed to fire a neuron.
+        G: Ratio of strong synapse strength to weak synapse strength, binary
+            approximation.
+        training: whether the neuron is in training mode.
+    """
+
+    def __init__(self, S0 = 16, H = 4.0, G = 2.0):
+        """Inits Neuron class.
+
+        Args:
+            fired_syn: List of input synapses that fired.
+        """
+        self.S0 = S0
+        self.H = H
+        self.G = G
+        self.strength = np.ones(S0)
+        self.training = False
+
+
+    def expose(self, w):
+        """Models how the neuron reacts to excitation patterns, and how it computes
+       whether or not to fire.
+
+       Expose computes the weighted sum of the input word, and the neuron fires if
+       that sum meets or exceeds a threshold. The weighted sum is the sum of the
+       S0 element-by-element products f the most recent neron vector, the current
+       word, and the neuron frozen Boolean vector.
+
+       Args:
+           w: A Word to present to the neuron.
+
+       Returns:
+           A Boolean indicating whether the neuron will fire or not.
+       """
+        # Compute the weighted sum of the firing inputs
+        s = self.strength[list(w.offset)].sum()
+        if self.training:
+            return s >= self.H
+        else:
+            return s >= self.H*self.G
+    
+    
+    def train(self, w):
+        """Trains a neuron with an input word.
+    
+       To train a neuron, "train" is called for each word to be recognized. If the
+       neuron fires for that word then all synapses that contributed to that
+       firing have their strengths irreversibly increased to G.
+
+       Args:
+           w: A Word to train the neuron with.
+    
+       Returns:
+           A Boolean indicating whether the neuron fired or not.
+       """
+        if not self.training:
+            print "[WARN] train(w) was called when not in training mode."
+        
+        if not self.expose(w): return False
+    
+        # Set the srength for participating synapses to G
+        self.strength[list(w.offset)] = self.G
+    
+        return True
+
+    
+    def start_training(self):
+        """Set the neuron in training mode.
+       """
+        self.training = True
+
+    def end_training(self):
+        """Set the neuron in recognition mode.
+    
+       Once the training is complete, the neuron's threshold value H is set
+       to H*G.
+       """
+        self.training = False
+
+
+### MAIN ###
+def main():
+    print ":: Cognon Basic model simulation ::"
+
+# Create a neuron model
+n = Neuron()
+
+# Create 3 training words
+wA = Word([1,6,9,14])
+wB = Word([3,4,9,13])
+wD = Word([2,6,12,14])
+wE = Word([3,7,9,13])
+wF = Word([1,4,9,14])
+
+if __name__ == '__main__':
+    main()
+
---
+++ test/test_cognon_basic.py
@@ -0,0 +1,95 @@
+# Copyright 2013 Pau Haro Negre
+# based on C++ code by Carl Staelin Copyright 2009-2011
+# 
+# See the NOTICE file distributed with this work for additional information
+# regarding copyright ownership.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from cognon_basic import Neuron
+from cognon_basic import Word
+
+from nose.tools import assert_false
+from nose.tools import assert_in
+from nose.tools import assert_true
+from nose.tools import eq_
+from nose.tools import raises
+
+
+class TestWord:
+
+    def test_empty(self):
+        w = Word()
+        eq_(len(w.offset), 0)
+
+    @raises(ValueError)
+    def test_negative_offset(self):
+        w = Word([-1])
+
+    def test_fire_1_3_8(self):
+        w = Word([1,3,8])
+        eq_(len(w.offset), 3)
+        assert_in(1, w.offset)
+        assert_in(3, w.offset)
+        assert_in(8, w.offset)
+
+
+class TestNeuron:
+
+    def test_defaults(self):
+        n = Neuron()
+        eq_(n.S0, 16)
+        eq_(n.H, 4.0)
+        eq_(n.G, 2.0)
+        eq_(len(n.strength), n.S0)
+        assert_false(n.training)
+
+    def test_expose_not_training(self):
+        n = Neuron(S0 = 16, H = 4.0, G = 2.0)
+
+        w1 = Word([1,6,9])
+        assert_false(n.expose(w1))
+
+        w2 = Word([1,3,4,5,6,8,9,14])
+        assert_true(n.expose(w2))
+
+    @raises(IndexError)
+    def test_expose_index_error(self):
+        n = Neuron(S0 = 16)
+        w = Word([16])
+        n.expose(w)
+
+    def test_train(self):
+        n = Neuron(16, 4.0, 2.0)
+        wA = Word([1,6,9,14])
+        wB = Word([3,4,9,13])
+
+        n.start_training()
+        assert_true(n.train(wA))
+        assert_true(n.train(wB))
+        n.end_training()
+
+        wD = Word([2,6,12,14])
+        wE = Word([3,7,9,13])
+        assert_false(n.expose(wD))
+        assert_false(n.expose(wE))
+
+        wF = Word([1,4,9,14])
+        assert_true(n.expose(wF))
+
+    def test_train_not_training(self):
+        n = Neuron()
+        w = Word()
+        assert_false(n.train(w))
+
