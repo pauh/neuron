b0b743d12be58b8812a90d4756eea8c6534111a3
Pau Haro Negre <pauharo@gmail.com>
2013-08-01 15:35:27 -0600

    Add Cognon class to run experiments with the model

--- run_experiment.py
+++ run_experiment.py
@@ -1,6 +1,6 @@
 # Copyright 2013 Pau Haro Negre
 # based on C++ code by Carl Staelin Copyright 2009-2011
-# 
+#
 # See the NOTICE file distributed with this work for additional information
 # regarding copyright ownership.
 #
@@ -28,7 +28,7 @@ class Alice(object):
 
     def train(self, neuron, wordset):
         neuron.start_training()
-        
+
         for word in wordset.words:
             fired = neuron.train(word)
 
@@ -61,4 +61,48 @@ class Bob(object):
                 self.false_true += 1
             else:
                 self.false_false += 1
+
+
+class Cognon(object):
+
+    def run_configuration(self, repetitions):
+
+        # Ensure that at least 10,000 words are learnt
+        min_learn_words = 10000
+        #N = repetitions
+        #if N * w < min_learn_words:
+        #    N = min_learn_words/w
+
+        # Ensure that at least 1,000,000 words are tested
+        min_test_words = 1000000
+        #K = min_test_words/N
+
+        #for i in xrange(N):
+        #    run_experiment(i)
+
+        # Config for each experiment:
+        # - neuron
+        #    S0, H, G, C, D1, D2, (num_active or refractory_period)
+        # - training
+        #    num_train_words (w)
+        # - test
+        #    num_test_words
+
+
+    def run_experiment(self):
+
+        # create a neuron instance with the provided parameters
+        neuron = Neuron(S0, H, G, C, D1, D2)
+
+        # create the training and test wordsets
+        train_wordset = WordSet(num_train_words, S0, D1, num_active)
+        test_wordset = WordSet(num_test_words, S0, D1, num_active)
+
+        # create Alice instance to train the neuron
+        alice = Alice()
+        alice.train(n, train_wordset)
+
+        # create a Bob instance to test the neuron
+        bob = Bob()
+        bob.test(neuron, train_wordset, test_wordset)
 
