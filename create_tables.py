c08f7033f954373617d7a58eb1e5b88f91ac1a27
Pau Haro Negre <pauharo@gmail.com>
2013-08-19 18:25:05 -0600

    Add creation of table 2.3. of the book

--- create_tables.py
+++ create_tables.py
@@ -25,8 +25,8 @@ def run_table_row(w, active, C, D1, D2, Q, R, G, H):
     repetitions = 20
 
     config = Configuration()
-    config.neuron_params(C, D1, D2, Q, R, G, H)
-    config.test_params(active, w, 5000)
+    config.neuron_params(C, D1, D2, Q, G, H)
+    config.test_params(active, R, w, 5000)
 
     cognon = Cognon()
     return cognon.run_configuration(config, repetitions)
@@ -56,7 +56,7 @@ def table21():
     table21_row(14, 10, 10000,  10, 1.5)
 
 def table21_row(N, H, S, w, G):
-    r = run_table_row(w, N, 1, 1, 1, S/float(H), 1, G, float(H))
+    r = run_table_row(w, N, 1, 1, 1, S/float(H), None, G, float(H))
     pF_mean = r['pF'].mean()*100
     pF_std  = r['pF'].std()*100
     L_mean  = r['L'].mean()
@@ -65,5 +65,43 @@ def table21_row(N, H, S, w, G):
     print txt.format(pF_mean, N, H, S, w, G, L_mean, L_S0)
 
 
+def table23():
+    print "%%%%%%%%%%%%%%"
+    print "% Table 2.3. %"
+    print "%%%%%%%%%%%%%%"
+    print
+              #  H |  G |  S0  |  R  | w #
+    table23_row( 30, 4.0, 10000, 303, 200)
+    table23_row(105, 4.0, 10000,  86,  70)
+    table23_row( 40, 1.9, 10000, 250, 100)
+
+    print "\t\\midrule"
+
+    table23_row(  5, 3.6,  1000, 333, 300)
+    table23_row( 10, 3.6,  1000, 111,  60)
+    table23_row(  5, 1.9,  1000, 333, 300)
+    table23_row( 15, 4.0,  1000,  66,  30)
+
+    print "\t\\midrule"
+
+    table23_row(  5, 3.6,   200,  57,  40)
+    table23_row( 10, 4.0,   200,  20,  10)
+    table23_row( 20, 1.9,   200,  12,  10)
+
+def table23_row(H, G, S, R, w):
+    r = run_table_row(w, None, 1, 1, 1, S/float(H*R), R, G, float(H))
+    L_mean  = r['L'].mean()
+    pF_mean = r['pF'].mean()*100
+    pF_std  = r['pF'].std()*100
+    pL_mean = r['pL'].mean()*100
+    pL_std  = r['pL'].std()*100
+    txt = "\t{:.1f} & {:.2f} & {:.2f} & {} & {:.1f} & {:,} & {} & {} \\\\"
+    print txt.format(L_mean, pF_mean, pL_mean, H, G, S, R, w)
+
+
+
 if __name__ == "__main__":
-    table21()
+    #table21()
+    #print
+    table23()
+
